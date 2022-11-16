206e9c60266e8d24552c182b9b7f3efa59045711
MJaycel <73226077+MJaycel@users.noreply.github.com>
2022-11-16 14:05:04 +0000

    CORS

--- server.js
+++ server.js
@@ -21,7 +21,17 @@ const { getAllVideos, getSingleVideo, addVideo, deleteVideo } = require('./contr
 const port = process.env.PORT || 3000
 
 const app = express()
-app.use(cors())
+
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
+// app.use(cors())
+
 app.use(express.json())
 
 // verify Token
